10c51e6a412efe5d2851f7bfb04ab47fa15ef34d
Lauri Pitkänen <lauri.m.pitkanen@utu.fi>
2020-02-13 15:02:30 +0200

    Fixed closing Game over window in idle.

--- roborun.py
+++ roborun.py
@@ -468,7 +468,7 @@ def game_over(player):
         for event in pygame.event.get():
             if (event.type == pygame.QUIT or
                 event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
-                return
+                return pygame.quit()
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_n:
                 lives = reset_lives
                 shoot_count = reset_fire
